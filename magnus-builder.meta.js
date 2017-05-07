97d372ee95b811c6a33a58b959bb1553a88262b3
Eccenux <eccenux+git@gmail.com>
2017-05-07 19:35:36 +0200

    version 0.0.4

--- magnus-builder.meta.js
+++ magnus-builder.meta.js
@@ -2,7 +2,7 @@
 // @id             iitc-plugin-magnus-builder@eccenux
 // @name           IITC plugin: Magnus builder tracker
 // @category       Misc
-// @version        0.0.3
+// @version        0.0.4
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @description    Allow manual entry of deployed, unique resonators. Use the 'highlighter-magnusBuilder' plugin to show the magnusBuilder on the map, and 'sync' to share between multiple browsers or desktop/mobile. It will try and guess which portals you have captured from portal details, but this will not catch every case.
 // @include        https://*.ingress.com/intel*
