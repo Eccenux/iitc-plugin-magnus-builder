c1d6e15f265d7b82fa0ad7f4fe4d03760e735aff
Eccenux <eccenux+git@gmail.com>
2017-05-12 00:41:04 +0200

    version in desc
    better for mobile

--- magnus-builder.user.js
+++ magnus-builder.user.js
@@ -4,7 +4,7 @@
 // @category       Misc
 // @version        0.1.1
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @description    Allow manual entry of deployed, unique resonators. Use the 'highlighter-magnusBuilder' plugin to show the magnusBuilder on the map, and 'sync' to share between multiple browsers or desktop/mobile. It will try and guess which portals you have captured from portal details, but this will not catch every case.
+// @description    [0.1.1] Allow manual entry of deployed, unique resonators. Use the 'highlighter-magnusBuilder' plugin to show the magnusBuilder on the map, and 'sync' to share between multiple browsers or desktop/mobile. It will try and guess which portals you have captured from portal details, but this will not catch every case.
 // @include        https://*.ingress.com/intel*
 // @include        http://*.ingress.com/intel*
 // @match          https://*.ingress.com/intel*
