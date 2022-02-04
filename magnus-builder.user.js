13431719d6d1f55d529176eb254fb79627edb05d
Nux <eccenux+git@gmail.com>
2022-02-04 23:17:33 +0100

    0.1.2: intel url update

--- magnus-builder.user.js
+++ magnus-builder.user.js
@@ -2,9 +2,9 @@
 // @id             iitc-plugin-magnus-builder@eccenux
 // @name           IITC plugin: Magnus builder tracker
 // @category       Misc
-// @version        0.1.1
+// @version        0.1.2
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @description    [0.1.1] Allow manual entry of deployed, unique resonators. Use the 'highlighter-magnusBuilder' plugin to show the magnusBuilder on the map, and 'sync' to share between multiple browsers or desktop/mobile. It will try and guess which portals you have captured from portal details, but this will not catch every case.
+// @description    [0.1.2] Allow manual entry of deployed, unique resonators. Use the 'highlighter-magnusBuilder' plugin to show the magnusBuilder on the map, and 'sync' to share between multiple browsers or desktop/mobile. It will try and guess which portals you have captured from portal details, but this will not catch every case.
 // @include        https://*.ingress.com/intel*
 // @include        http://*.ingress.com/intel*
 // @match          https://*.ingress.com/intel*
@@ -13,6 +13,7 @@
 // @include        http://*.ingress.com/mission/*
 // @match          https://*.ingress.com/mission/*
 // @match          http://*.ingress.com/mission/*
+// @match          https://intel.ingress.com/*
 // @grant          none
 // @updateURL      https://github.com/Eccenux/iitc-plugin-magnus-builder/raw/master/magnus-builder.meta.js
 // @downloadURL    https://github.com/Eccenux/iitc-plugin-magnus-builder/raw/master/magnus-builder.user.js
